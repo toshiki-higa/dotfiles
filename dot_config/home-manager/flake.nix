21075f916e8d50224ba52afa43e2375752f49608
toshiki-higa <127714325+toshiki-higa@users.noreply.github.com>
2026-08-13 14:11:32 +0900

    fix(nix): update mcpx binary cache URL

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -4,7 +4,7 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.numtide.com"
-      "https://yata-one.github.io/mcpx"
+      "https://yatainc.github.io/mcpx"
     ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
